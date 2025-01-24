1b15e60936392cebc0efdf1690c237a60224daaa
Matthieu Fouquet <mfouquet@sqli.com>
2025-01-24 09:22:28 +0100

    add more unit tests for API

--- RabbitMqProductAPI.Tests/Services/ProductServiceTests.cs
+++ RabbitMqProductAPI.Tests/Services/ProductServiceTests.cs
@@ -63,5 +63,90 @@ namespace RabbitMqProductAPI.Tests.Services
             result.Should().BeEquivalentTo(expectedProduct);
             _productServiceMock.Verify(service => service.GetProductById(expectedProduct.ProductId), Times.Once);
         }
+
+        [Fact]
+        public void GetProductById_ShouldReturnNull_WhenProductDoesNotExist()
+        {
+            // Arrange
+            var productId = _fixture.Create<int>();
+            _productServiceMock
+                .Setup(service => service.GetProductById(productId))
+                .Returns((Product)null);
+
+            var sut = _fixture.Create<IProductService>();
+
+            // Act
+            var result = sut.GetProductById(productId);
+
+            // Assert
+            result.Should().BeNull();
+            _productServiceMock.Verify(service => service.GetProductById(productId), Times.Once);
+        }
+
+
+        [Fact]
+        public void DeleteProduct_ShouldReturnFalse_WhenProductDoesNotExist()
+        {
+            // Arrange
+            var productId = _fixture.Create<int>();
+            _productServiceMock
+                .Setup(service => service.DeleteProduct(productId))
+                .Returns(false);
+
+            var sut = _fixture.Create<IProductService>();
+
+            // Act
+            var result = sut.DeleteProduct(productId);
+
+            // Assert
+            result.Should().BeFalse();
+            _productServiceMock.Verify(service => service.DeleteProduct(productId), Times.Once);
+        }
+
+        [Fact]
+        public void UpdateProduct_ShouldUpdateOnlyModifiedFields()
+        {
+            // Arrange
+            var existingProduct = _fixture.Build<Product>()
+                                          .With(p => p.ProductName, "Old Name")
+                                          .Create();
+
+            var updatedProduct = existingProduct;
+            updatedProduct.ProductName = "New Name";
+
+            _productServiceMock
+                .Setup(service => service.UpdateProduct(updatedProduct))
+                .Returns(updatedProduct);
+
+            var sut = _fixture.Create<IProductService>();
+
+            // Act
+            var result = sut.UpdateProduct(updatedProduct);
+
+            // Assert
+            result.ProductName.Should().Be("New Name");
+            result.ProductDescription.Should().Be(existingProduct.ProductDescription);
+            _productServiceMock.Verify(service => service.UpdateProduct(updatedProduct), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteProduct_ShouldThrowException_WhenUnexpectedErrorOccurs()
+        {
+            // Arrange
+            var productId = _fixture.Create<int>();
+            _productServiceMock
+                .Setup(service => service.DeleteProduct(productId))
+                .Throws(new Exception("Unexpected error"));
+
+            var sut = _fixture.Create<IProductService>();
+
+            // Act
+            var act = () => sut.DeleteProduct(productId);
+
+            // Assert
+            act.Should().Throw<Exception>()
+                .WithMessage("Unexpected error");
+            _productServiceMock.Verify(service => service.DeleteProduct(productId), Times.Once);
+        }
     }
 }
